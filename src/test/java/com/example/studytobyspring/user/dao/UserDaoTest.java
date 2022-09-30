ca2fade78bed2baa75debab8e70d858ab4afd4e4
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-30 15:56:07 +0900

    feat: count 추가

--- src/test/java/com/example/studytobyspring/user/dao/UserDaoTest.java
+++ src/test/java/com/example/studytobyspring/user/dao/UserDaoTest.java
@@ -39,4 +39,22 @@ class UserDaoTest {
         assertThat(findUser.getName()).isEqualTo(savedUser.getName());
     }
 
+    @Test
+    void count() throws Exception{
+        User user1 = new User("gyumme", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
+        dao.add(user1);
+        assertThat(dao.getCount()).isEqualTo(1);
+
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
+
+        dao.add(user3);
+        assertThat(dao.getCount()).isEqualTo(3);
+    }
 }
