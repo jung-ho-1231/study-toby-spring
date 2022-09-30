1bc84d638ca904ca254302c943f8184450994ab1
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-30 15:31:45 +0900

    feat: UserDaoTest 에 addAndGet 추가

--- src/test/java/com/example/studytobyspring/user/dao/UserDaoTest.java
+++ src/test/java/com/example/studytobyspring/user/dao/UserDaoTest.java
@@ -1,14 +1,16 @@
 package com.example.studytobyspring.user.dao;
 
+import com.example.studytobyspring.user.domain.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+//@SpringBootTest
 class UserDaoTest {
 
     ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
@@ -16,6 +18,22 @@ class UserDaoTest {
     @Test
     void userDaoTest() throws Exception{
         UserDao userDao = context.getBean("userDao", UserDao.class);
-        Assertions.assertThat(userDao).isNotNull();
+        assertThat(userDao).isNotNull();
+    }
+
+    @Test
+    void addAndGet() throws Exception{
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User savedUser = new User();
+        savedUser.setId("park");
+        savedUser.setName("jungho");
+        savedUser.setPassword("1231");
+
+        dao.add(savedUser);
+
+        User findUser = dao.get(savedUser.getId());
+
+        assertThat(findUser.getName()).isEqualTo(savedUser.getName());
     }
 }
