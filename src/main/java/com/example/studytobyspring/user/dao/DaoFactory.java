a0a3c3f894fbc979997e9c6eadd3dee4d03ff89a
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-30 12:46:46 +0900

    refactor: DaoFactory 수정

--- src/main/java/com/example/studytobyspring/user/dao/DaoFactory.java
+++ src/main/java/com/example/studytobyspring/user/dao/DaoFactory.java
@@ -1,18 +1,16 @@
 package com.example.studytobyspring.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
-    public AccountDao accountDao() {
-        return new AccountDao(connectionMaker());
-    }
-
-    public MessageDao messageDao() {
-        return new MessageDao(connectionMaker());
-    }
-
+    @Bean
     public static DConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
